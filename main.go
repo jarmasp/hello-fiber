682e6b5255ed5959a1c18dcd3c003eb1ef8c1084
“Jose <armasjose606@gmail.com>
2024-01-06 09:42:53 -0400

    Feat: fiber hello world

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/gofiber/fiber/v2"
+
 func main() {
-	println("hello world")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world")
+	})
+
+	app.Listen(":3000")
 }
