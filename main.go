d79ea8e4fb26dbccdb8cd42ca1b3d08a13dae288
“Jose <armasjose606@gmail.com>
2024-01-06 17:12:38 -0400

    Add: get user Add: post user and return with uuid

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        string
+	Firstname string
+	Lastname  string
+}
+
+func handleUser(c *fiber.Ctx) error {
+	user := User{
+		Firstname: "Jane",
+		Lastname:  "Doe",
+	}
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func handleCreateUser(c *fiber.Ctx) error {
+	user := User{}
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+	user.Id = uuid.NewString()
+	return c.Status(fiber.StatusOK).JSON(user)
+}
 
 func main() {
 	app := fiber.New()
@@ -8,6 +34,7 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world")
 	})
-
+	app.Get("/user", handleUser)
+	app.Post("/user", handleCreateUser)
 	app.Listen(":3000")
 }
